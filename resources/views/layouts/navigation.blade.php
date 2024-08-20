babb6d326f6d6c4a2ab70aabd9240c4cd4f1b03e
justbepanda <justbepanda@gmail.com>
2024-08-20 13:22:50 +0300

    add menu items to mobile view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -97,13 +97,17 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('task_statuses.index')" :active="request()->routeIs('task_statuses.index')">
+                {{ __('Task statuses') }}
+            </x-responsive-nav-link>
+        </div>
         @if (Route::has('login'))
             @auth
-                <div class="pt-2 pb-3 space-y-1">
-                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-responsive-nav-link>
-                </div>
+
 
 
                 <!-- Responsive Settings Options -->
