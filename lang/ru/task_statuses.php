3773bdd720d893f4ccc10b5a881aaf8f00678574
justbepanda <justbepanda@gmail.com>
2024-08-30 13:42:53 +0300

    change translate

--- lang/ru/task_statuses.php
+++ lang/ru/task_statuses.php
@@ -17,8 +17,8 @@ return [
     'There are no task statuses' => 'Нет статусов задач',
     'Task status' => 'Статус задачи',
     'Status created successfully!' => 'Статус успешно создан!',
-    'Status updated successfully!' => 'Статус успешно обновлен!',
-    'Status deleted successfully!' => 'Статус успешно удален!',
+    'Status updated successfully!' => 'Статус успешно изменён!',
+    'Status deleted successfully!' => 'Статус успешно удалён!',
     'The status cannot be deleted because it is associated with tasks.' => 'Статус не может быть удален, так как он связан с задачами.',
     'Error when deleting the task status: ' => 'Ошибка при удалении статуса задачи: ',
 ];
--- resources/views/task_statuses/create.blade.php
+++ resources/views/task_statuses/create.blade.php
@@ -14,7 +14,7 @@
                         <!-- Name -->
                         <div>
                             <x-input-label for="name" :value="__('task_statuses.Name')" />
-                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" autofocus autocomplete="name" />
                             <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
 
