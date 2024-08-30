9690b3690961435cd31d0e320fa59a2506303a20
justbepanda <justbepanda@gmail.com>
2024-08-30 08:33:43 +0300

    change lang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,9 +25,15 @@
 
 
     </nav>
-
-
 </header>
-
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                {{ __("messages.Welcome") }}
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
